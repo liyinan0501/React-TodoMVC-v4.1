7f9e61de5c7f43d659a60072778b20126cd1a3ef
Yinan Li <gabriel.li5566@gmail.com>
2022-08-11 14:19:15 +0300

    add delete todo function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,17 @@
+import { useDispatch } from 'react-redux'
+import { delTodo } from 'store/actions'
+
 export default function TodoItem({ item }) {
+  const dispatch = useDispatch()
+  const del = (id) => {
+    dispatch(delTodo(id))
+  }
   return (
     <li className={item.done ? 'completed' : ''}>
       <div className="view">
         <input className="toggle" type="checkbox" checked={item.done} />
         <label>{item.name}</label>
-        <button className="destroy" />
+        <button className="destroy" onClick={() => del(item.id)} />
       </div>
       <input className="edit" defaultValue="Create a TodoMVC template" />
     </li>
