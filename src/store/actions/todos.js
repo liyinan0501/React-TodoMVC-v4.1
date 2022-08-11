6e42aa8e0ad56979db2bef5daf60a8972087535a
Yinan Li <gabriel.li5566@gmail.com>
2022-08-11 14:19:47 +0300

    update add and delete todo action

--- src/store/actions/todos.js
+++ src/store/actions/todos.js
@@ -5,7 +5,30 @@ export const getList = () => {
     const res = await axios.get('http://localhost:8888/todos')
     dispatch({
       type: 'GET_LIST',
-      list: res.data,
+      payload: res.data,
+    })
+  }
+}
+
+export const addTodo = (name) => {
+  return async (dispatch) => {
+    const res = await axios.post('http://localhost:8888/todos', {
+      name,
+      done: false,
+    })
+    dispatch({
+      type: 'ADD_TODO',
+      payload: res.data,
+    })
+  }
+}
+
+export const delTodo = (id) => {
+  return async (dispatch) => {
+    const res = await axios.delete(`http://localhost:8888/todos/${id}`)
+    dispatch({
+      type: 'DEL_TODO',
+      payload: id,
     })
   }
 }
