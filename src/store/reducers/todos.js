027ca8fb874621229cd3f0c41edcab2e78a24df5
Yinan Li <gabriel.li5566@gmail.com>
2022-08-11 14:19:55 +0300

    update add and delete todo reducer

--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -1,7 +1,14 @@
 export default function todos(state = [], action) {
-  switch (action.type) {
+  const { type, payload } = action
+  switch (type) {
     case 'GET_LIST':
-      return action.list
+      return payload
+
+    case 'ADD_TODO':
+      return [...state, payload]
+
+    case 'DEL_TODO':
+      return state.filter((item) => item.id !== payload)
 
     default:
       return state
